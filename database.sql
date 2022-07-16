1135d122f89f8900ffba048940561abb568a1958
Arthur Lapz <rlapz@gnuweeb.org>
2022-07-16 20:03:53 +0700

    database.sql: Add `unique key` for `todo.title`

--- database.sql
+++ database.sql
@@ -1,92 +1,46 @@
--- MariaDB dump 10.19  Distrib 10.8.3-MariaDB, for Linux (x86_64)
---
--- Host: localhost    Database: dooo
--- ------------------------------------------------------
--- Server version	10.8.3-MariaDB
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `todo`
---
-
-DROP TABLE IF EXISTS `todo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `todo` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) unsigned NOT NULL,
-  `title` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `detail` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status` enum('p','f','s') COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `title` (`title`),
-  KEY `status` (`status`),
-  CONSTRAINT `todo_fk1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `todo`
---
-
-LOCK TABLES `todo` WRITE;
-/*!40000 ALTER TABLE `todo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `todo` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status` enum('b','d','e') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  KEY `first_name` (`first_name`),
-  KEY `last_name` (`last_name`),
-  KEY `status` (`status`),
-  KEY `created_at` (`created_at`),
-  KEY `updated_at` (`updated_at`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+	`id`         bigint unsigned     NOT NULL AUTO_INCREMENT,
+	`first_name` varchar(64)         NOT NULL COLLATE utf8mb4_unicode_ci,
+	`last_name`  varchar(64)         NOT NULL COLLATE utf8mb4_unicode_ci,
+	`username`   varchar(16)         NOT NULL COLLATE utf8mb4_unicode_ci,
+	`password`   text                NOT NULL COLLATE utf8mb4_unicode_ci,
+	`status`     enum('b', 'd', 'e') NOT NULL,
+	`created_at` datetime            NOT NULL,
+	`updated_at` datetime            DEFAULT NULL,
 
---
--- Dumping data for table `users`
---
+	PRIMARY KEY (`id`),
+	UNIQUE  KEY `username`  (`username`),
+	KEY         `first_name`(`first_name`),
+	KEY         `last_name` (`last_name`),
+	KEY         `status`    (`status`),
+	KEY         `created_at`(`created_at`),
+	KEY         `updated_at`(`updated_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-16 10:22:57
+DROP TABLE IF EXISTS `todo`;
+CREATE TABLE `todo` (
+	`id`      bigint unsigned     NOT NULL AUTO_INCREMENT,
+	`user_id` bigint unsigned     NOT NULL,
+	`title`   varchar(32)         NOT NULL COLLATE utf8mb4_unicode_ci,
+	`detail`  text                DEFAULT NULL COLLATE utf8mb4_unicode_ci,
+	`status`  enum('p', 'f', 's') NOT NULL COLLATE utf8mb4_unicode_ci,
+	PRIMARY KEY(`id`),
+	KEY        `user_id` (`user_id`),
+	UNIQUE KEY `title`   (`title`),
+	KEY        `status`  (`status`),
+
+	CONSTRAINT `todo_fk1` FOREIGN KEY (`user_id`)
+		REFERENCES `users` (`id`)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+LOCK TABLES `todo` WRITE;
+UNLOCK TABLES;
