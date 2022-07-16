81e86a4ffc7e852450954f91f182d46278fde78b
Arthur Lapz <rlapz@gnuweeb.org>
2022-07-16 10:24:28 +0700

    api: Add "test" mariadb

--- src/controllers/apis/v1.js
+++ src/controllers/apis/v1.js
@@ -1,4 +1,18 @@
+const db = require("../../db/mariadb");
+
+
+/* Test */
 module.exports = async (_, res) => {
-	await res.json({status: true});
+	let row;
+
+	try {
+		row = await db.pool.query("SELECT NOW() AS NOW");
+	} catch (err) {
+		await res.json({status: false, msg: `${err}`});
+		console.error(err);
+		return;
+	}
+
+	await res.json({status: true, now: row[0].NOW});
 };
 
