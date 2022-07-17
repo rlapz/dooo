b106e82b7ba5760286f659c38411e8e25046e478
Arthur Lapz <rlapz@gnuweeb.org>
2022-07-17 10:40:58 +0700

    api: Using proper unhandled api path

--- src/routes/api.js
+++ src/routes/api.js
@@ -6,6 +6,12 @@ const v1 = require("./apis/v1");
 const router = express.Router();
 
 router.use("/v1", v1);
+router.use(async (_, res) => {
+	await res.status(400).json({
+		status: 400,
+		result: "Bad Request!"
+	});
+});
 
 
 module.exports = router;
--- src/routes/apis/v1.js
+++ src/routes/apis/v1.js
@@ -7,12 +7,6 @@ const router = express.Router();
 
 router.get("/user", capi.get_user);
 router.get("/user/:id", capi.get_user);
-router.use(async (_, res) => {
-	await res.json({
-		status: 404,
-		result: "Not Found!"
-	});
-});
 
 
 module.exports = router;
