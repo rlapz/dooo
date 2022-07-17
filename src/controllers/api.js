f96f8da21a96286424c05f9e887e185c0902d80c
Arthur Lapz <rlapz@gnuweeb.org>
2022-07-18 06:23:06 +0700

    api: Re-design api function handler.

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -5,32 +5,16 @@ const get_user = async (req, res) => {
 	const id = req.params.id;
 	let ret = {
 		status: 200,
-		result: null
+		rows: 0,
+		result: []
 	};
 
+	if (id)
+		ret.result = await user_model.get_by_id(id);
+	else
+		ret.result = await user_model.get_all();
 
-	try {
-		const res = await user_model.select(id);
-
-
-		if (!res[0])
-			throw "No Data";
-
-		ret.result = [];
-
-		for (let {first_name, last_name, username, status} of res) {
-			ret.result.push({
-				first_name,
-				last_name,
-				username,
-				status,
-			});
-		}
-
-	} catch (err) {
-		ret.status = 404;
-		ret.result = err;
-	}
+	ret.rows = ret.result.length;
 
 	await res.status(ret.status).json(ret);
 };
--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,39 @@
-const db = require("../db/mariadb");
+const db = require("../db/mariadb").pool;
 
 
-const select = async (id) => {
-	if (!id)
-		return db.pool.query("SELECT * FROM users;");
+const sql_select = "SELECT first_name, last_name, username, status FROM users";
 
-	return db.pool.query("SELECT * FROM users WHERE `id` = ?;", [id]);
+
+const get_all = async () => {
+	try {
+		const res = await db.query({bigIntAsNumber: true, sql: sql_select});
+
+		return res.slice(0);
+	} catch (err) {
+		console.error(`models.user.get_all: ${err}`);
+	}
+
+	return [];
 }
 
 
+const get_by_id = async (id) => {
+	const sql1 = `${sql_select} WHERE \`id\` = ?`;
+
+
+	try {
+		const res = await db.query({bigIntAsNumber: true, sql: sql1}, [id]);
+
+		return res.slice(0);
+	} catch (err) {
+		console.error(`models.user.get_by_id: ${err}`);
+	};
+
+	return [];
+};
+
+
 module.exports = {
-	select,
+	get_all,
+	get_by_id
 }
