c2d0db0344e634ed64e620908e941b5f51c87467
Arthur Lapz <rlapz@gnuweeb.org>
2022-07-23 03:15:14 +0700

    api: Add validator for `user`

--- src/controllers/apis/user.js
+++ src/controllers/apis/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const user_model = require("../../models/user");
 const err = require("../../utils/api_error");
@@ -26,20 +27,45 @@ const sign_up = async (req, res) => {
 	let {first_name, last_name, username, email, password} = req.body;
 
 
-	if (!first_name)
+	if (!first_name || (first_name = first_name.trim()).length == 0)
 		return err.bad_request(res, "'first_name' is empty!");
+
+	const first_name_len = first_name.length;
+	if (first_name_len > 64)
+		return err.bad_request(res, "'first_name' is too long, max: 64!");
 
 	if (!last_name)
 		last_name = null;
 
-	if (!username)
+	if (last_name && last_name.length > 64)
+		return err.bad_request(res, "'last_name' is too long, max: 64!");
+
+	if (!username || (username = username.trim()).length == 0)
 		return err.bad_request(res, "'username' is empty!");
 
-	if (!email)
+	const username_len = username.length;
+	if (username_len > 16)
+		return err.bad_request(res, "'username' is too long, max: 16!");
+
+	if (username_len < 3)
+		return err.bad_request(res, "'username' is too short, min: 3!");
+
+	if (!email || (email = email.trim()) == 0)
 		return err.bad_request(res, "'email' is empty!");
 
-	if (!password)
+	if (!validator.isEmail(email))
+		return err.bad_request(res, "Invalid 'email'!");
+
+	if (!password || (password = password.trim()).length == 0)
 		return err.bad_request(res, "'password' is empty!");
+
+	const password_len = password.length;
+	if (password_len > 255)
+		return err.bad_request(res, "'password' is too long, max: 255!");
+
+	if (password_len < 6)
+		return err.bad_request(res, "'password' is too short, min: 6!");
+
 
 	let h_pass;
 	try {
@@ -70,13 +96,26 @@ const sign_up = async (req, res) => {
 const sign_in = async (req, res) => {
 	let {username, password} = req.body;
 
-	if (!username)
+
+	if (!username || (username = username.trim()).length == 0)
 		return err.unauthorized(res, "Username is empty!");
 
-	username = username.trim();
+	const username_len = username.length;
+	if (username_len > 16)
+		return err.unauthorized(res, "Username is too long, max: 16!");
 
-	if (!password)
+	if (username_len < 3)
+		return err.unauthorized(res, "Username is too short, max: 3!");
+
+	if (!password || (password = password.trim()).length == 0)
 		return err.unauthorized(res, "Password is empty!");
+
+	const password_len = password.length;
+	if (password_len > 255)
+		return err.unauthorized(res, "Password is too long, max: 255!");
+
+	if (password_len < 6)
+		return err.unauthorized(res, "Password is too short, min: 6!");
 
 	const _res = await user_model.sign_in(username);
 	if (!_res.status)
